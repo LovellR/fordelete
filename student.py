99b11e6bfc48e34b0636f2bc6b05b966beb48be5
LovellR <129665363+LovellR@users.noreply.github.com>
2024-05-31 12:31:29 +0900

    practice

---
+++ student.py
@@ -0,0 +1,10 @@
+
+def print_student_info():
+    student_id = "2021041099"  # 학번
+    name = "이준호"         # 이름
+
+    print(f"학번: {student_id}")
+    print(f"이름: {name}")
+
+if __name__ == "__main__":
+    print_student_info()
